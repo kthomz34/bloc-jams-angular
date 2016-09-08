8190eab96c4a8560d907fe51b4d63f78815fa07d
Kurt Thomas <Kurt Thomas>
2016-09-08 16:07:39 -0400

    A6 complete. Added collection fixture

--- dist/scripts/services/Fixtures.js
+++ dist/scripts/services/Fixtures.js
@@ -36,8 +36,16 @@
             return albumPicasso;  
         };
         
+        Fixtures.getCollection = function(numberOfAlbums) {
+            var albums = [];
+            for (var i = 0; i < numberOfAlbums; i++){
+                albums.push(albumPicasso);
+            }
+            return albums;
+        };
+        
         return Fixtures;
-    }
+    };
     
     angular
         .module('blocjams')
